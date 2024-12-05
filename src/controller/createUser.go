557b139ecbe3014382ef9189b03a3dad1d733095
michael-andryeer <andryeerm@gmail.com>
2024-12-05 02:43:24 -0300

    Criando objeto usuário

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,12 +1,23 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/Michael-Andryeer/crud-go/src/configuration/rest_errors"
+	"github.com/Michael-Andryeer/crud-go/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
 
-	err := rest_errors.NewInternalServerError("Error creating user")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_errors.NewBadRequestError(
+			fmt.Sprintf("There are some incorret fields, error=%s\n", err.Error()),
+		)
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(userRequest)
 }
