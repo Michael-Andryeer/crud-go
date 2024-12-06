2129d134ddb25a99fe129e31372e643a3924a7f7
michael-andryeer <andryeerm@gmail.com>
2024-12-06 02:53:06 -0300

    Adicionando logs no projeto

--- go.mod
+++ go.mod
@@ -24,6 +24,8 @@ require (
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/arch v0.12.0 // indirect
 	golang.org/x/crypto v0.30.0 // indirect
 	golang.org/x/net v0.32.0 // indirect
@@ -31,4 +33,5 @@ require (
 	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/protobuf v1.35.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/gorm v1.25.12
 )
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,21 +1,46 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
+	"github.com/Michael-Andryeer/crud-go/src/configuration/logger"
 	"github.com/Michael-Andryeer/crud-go/src/configuration/validation"
 	"github.com/Michael-Andryeer/crud-go/src/controller/model/request"
+	"github.com/Michael-Andryeer/crud-go/src/controller/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap/zapcore"
 )
 
 func CreateUser(c *gin.Context) {
-
+	logger.Info("Init CreateUser Controller",
+		zapcore.Field{
+			Key:    "journey",
+			String: "createUser",
+		},
+	)
 	var userRequest request.UserRequest
-
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err,
+			zapcore.Field{
+				Key:    "journey",
+				String: "createUser",
+			},
+		)
 		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
-	fmt.Println(userRequest)
+	response := response.UserResponse{
+		Id:    "Test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+	logger.Info("User created successfully",
+		zapcore.Field{
+			Key:    "journey",
+			String: "createUser",
+		},
+	)
+	c.JSON(http.StatusOK, response)
 }
