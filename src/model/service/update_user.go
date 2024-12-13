db4a7e3941c393e09f7a5d57bd0095964decc492
michael-andryeer <andryeerm@gmail.com>
2024-12-13 18:54:01 -0300

    Criando Service

--- src/model/service/update_user.go
+++ src/model/service/update_user.go
@@ -1,10 +1,33 @@
 package service
 
 import (
+	"github.com/Michael-Andryeer/crud-go/src/configuration/logger"
 	"github.com/Michael-Andryeer/crud-go/src/configuration/rest_errors"
 	"github.com/Michael-Andryeer/crud-go/src/model"
+	"go.uber.org/zap"
 )
 
-func (*userDomainService) UpdateUser(userId string, userDomain model.UserDomainInterface) *rest_errors.RestError {
+func (ud *userDomainService) UpdateUser(
+	userId string,
+	userDomain model.UserDomainInterface,
+) *rest_errors.RestError {
+	logger.Info(
+		"Init updateUser model.",
+		zap.String("journey", "updateUser"),
+	)
+
+	err := ud.userRepository.(userDomain)
+	if err != nil {
+		logger.Error("Error trying to call repository",
+			err,
+			zap.String("journey", "updateUser"),
+		)
+		return nil, err
+	}
+
+	logger.Info("updateUser model executed successfully",
+		zap.String("userId", userId),
+		zap.String("journey", "updateUser"),
+	)
 	return nil
 }
